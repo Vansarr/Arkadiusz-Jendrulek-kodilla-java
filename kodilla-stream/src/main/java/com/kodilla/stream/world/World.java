f314a8b3bcaa54ab670561c30c3d57c5dbbf6307
Arkadiusz Jendrulek <ajendrulek@gmail.com>
2022-04-26 18:24:07 +0200

    7.4.0 World population counting

--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public final class World {
 
-    private List<Continent> continents = new ArrayList<>();
+    private final List<Continent> continents = new ArrayList<>();
 
 public void addContinent(Continent continent) {
     continents.add(continent);
